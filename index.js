8114edabbce9106ef7307d030ff876fcfe757e00
ImranHossain1 <imranhossain1402@gmail.com>
2022-05-09 20:30:56 +0200

    added get request for appointment

--- index.js
+++ index.js
@@ -6,14 +6,30 @@ const port = process.env.PORT || 5000
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(cors());
-
+app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m9khx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run(){
     try{
         await client.connect();
-        console.log('database connected successfully');
-    }
+        const appointmentCollection = client.db('doctors_portal').collection('appointments');
+        app.get('/appointments', async(req, res)=>{
+          const email = req.query.email;
+          const date = new Date(req.query.date).toLocaleDateString();
+          // console.log(date);
+          const query = {email: email, date: date};
+
+          const cursor = appointmentCollection.find(query);
+          const appointments = await cursor.toArray();
+          res.json(appointments);
+        })
+
+        app.post('/appointments', async(req, res)=>{
+          const appointment = req.body;
+          const result = await appointmentCollection.insertOne(appointment);
+          res.json(result);
+        });
+      }
     finally{
         // await client.close();
     }
