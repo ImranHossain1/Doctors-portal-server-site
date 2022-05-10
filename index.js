4fde2f3910de8ff66085760825040bc21454adef
ImranHossain1 <imranhossain1402@gmail.com>
2022-05-10 11:44:27 +0200

    added put request

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ async function run(){
     try{
         await client.connect();
         const appointmentCollection = client.db('doctors_portal').collection('appointments');
+        const usersCollection = client.db('doctors_portal').collection('users');
         app.get('/appointments', async(req, res)=>{
           const email = req.query.email;
           const date = new Date(req.query.date).toLocaleDateString();
@@ -29,6 +30,42 @@ async function run(){
           const result = await appointmentCollection.insertOne(appointment);
           res.json(result);
         });
+
+        app.get('/users/:email', async(req, res)=>{
+          const email= req.params.email;
+          const query ={ email: email};
+          const user = await usersCollection.findOne(query);
+          let isAdmin = false;
+          if(user?.role === 'admin'){
+            isAdmin= true;
+          }
+          res.json({admin: isAdmin});
+        })
+
+        app.post('/users', async(req,res)=>{
+          const user = req.body;
+          const result = await usersCollection.insertOne(user);
+          res.json(result);
+        })
+
+        app.put('/users', async(req,res)=>{
+          const user = req.body;
+          const filter = {email: user.email};
+          const options = { upsert: true };
+          const updateDoc = { $set : user};
+          const result = await usersCollection.updateOne(filter, updateDoc, options);
+          res.json(result);
+
+        })
+
+        app.put('/users/admin',async(req, res)=>{
+          const user = req.body;
+          console.log('put', user);
+          const filter = {email: user.email};
+          const updateDoc = {$set : {role: 'admin'}};
+          const result = await usersCollection.updateOne(filter, updateDoc);
+          res.json(result)
+        })
       }
     finally{
         // await client.close();
